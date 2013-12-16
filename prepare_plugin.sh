f9dd5d95f35df5680bbf164ba14eb3d3e8ce869f
alfredolainez <alfredo.lainez@mnopi.com>
2013-12-16 17:04:28 +0100

    Improvement in plugin shipping script

--- prepare_plugin.sh
+++ prepare_plugin.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+VERSION=alpha2
 
 cp -r !(prepare_plugin.sh) ../extension_mnopi
 
 cd ..
-zip -r mnopiBackend/mnopi/static/mnopi/plugin-mnopi-v0.1.zip extension_mnopi
+zip -r mnopiBackend/mnopi/static/mnopi/plugin-mnopi-$VERSION.zip extension_mnopi
