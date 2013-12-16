03e2b7740e8c3101251a1f66edb33ad762263cd9
alfredolainez <alfredo.lainez@mnopi.com>
2013-12-16 17:05:20 +0100

    New server data

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,6 @@
 // Server and POST values
-var MNOPI_SERVER_URL = "http://192.168.1.3:8000/";
-//var MNOPI_SERVER_URL = "http://ec2-184-73-11-181.compute-1.amazonaws.com/";
+//var MNOPI_SERVER_URL = "http://192.168.1.3:8000/";
+var MNOPI_SERVER_URL = "http://ec2-107-22-25-87.compute-1.amazonaws.com/";
 
 var POST_SERVICES = {
     'login' : "plugin_login",
